c85b9a4e615a1bbedd9da569bac60a17228a7c2d
jin_hyun89 <jin_hyun89@naver.com>
2016-09-13 02:01:23 +0900

    EJS/Count test

--- app.js
+++ app.js
@@ -2,8 +2,29 @@
   var path = require('path');
   var app = express();
 
+  app.set("view engine", 'ejs');
   app.use(express.static(path.join(__dirname + '/public')));
 
+  var data={count:0};
+  app.get('/', function (req,res) {
+    data.count++;
+    res.render('ejs',data);
+  });
+  app.get('/reset', function (req,res) {
+    data.count=0;
+    res.render('ejs',data);
+  });
+  app.get('/set/count', function (req,res) {
+    if(req.query.count) data.count=req.query.count;
+    res.render('ejs',data);
+  });
+  app.get('/set/:num', function (req,res) {
+    data.count=req.params.num;
+    res.render('ejs',data);
+  });
+  app.get('/', function (req,res) {
+    res.render('ejs');
+  });
   app.get('/',function (req,res) {
     res.send('Hello World!');
   });
